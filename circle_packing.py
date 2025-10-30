152f757e7a28d8487bc25c29f3bffa557d50d0bb
Jonel Relucio <jonel.relucio@mail.polimi.it>
2025-10-30 08:23:44 +0100

    Refactoring

--- circle_packing.py
+++ circle_packing.py
@@ -11,13 +11,14 @@ class CirclePacking():
         "lgo" : "/home/hojo/amplide/ampl.linux-intel64/lgo",
         "lindoglobal" : "/home/hojo/amplide/ampl.linux-intel64/lindoglobal", 
         "octeract": "/home/hojo/amplide/ampl.linux-intel64/octeract",
-        "couenne": "/home/hojo/amplide/ampl.linux-intel64/couenne"
+        "couenne": "/home/hojo/amplide/ampl.linux-intel64/couenne",
+        "knitro": "/home/hojo/amplide/ampl.linux-intel64/knitro"
     }
-    
+
 
     def __init__(self, n=2, solver="octeract"):
-        self.x_min, self.x_max = 0.0, 10.0
-        self.y_min, self.y_max = 0.0, 10.0
+        self.x_min, self.x_max = 0.0, 1.0
+        self.y_min, self.y_max = 0.0, 1.0
 
         self.circle_centers = []
         self.radius = 0.0
@@ -30,28 +31,31 @@ class CirclePacking():
             print(f"Choose between: ipopt, baron, lgo, lindoglobal, octeract, couenne")
 
 
+
     def optimize(self):
-        with open("circle_packing.mod", "r") as file:
-            ampl_code = file.read()
 
         ampl = AMPL(Environment())
-        ampl.eval(ampl_code)
-        ampl.param['n'].set(circle_packing.num_circles)
-        ampl.param['Xmin'].set(self.x_min)
-        ampl.param['Xmax'].set(self.x_max)
-        ampl.param['Ymin'].set(self.y_min)
-        ampl.param['Ymax'].set(self.y_max)
-
+        ampl.read("circle_packing.mod")
+        ampl.read_data("circle_packing.dat")
         ampl.setOption('solver', circle_packing.solver)
         ampl.solve()
 
-        for i in range(1, circle_packing.num_circles+1):
+        self.update_params(ampl)
+
+
+    def update_params(self, ampl):
+        self.num_circles = ampl.param['n'].value()
+        self.x_min = ampl.param['Xmin'].value()
+        self.x_max = ampl.param['Xmax'].value()
+        self.y_min = ampl.param['Ymin'].value()
+        self.y_max = ampl.param['Ymax'].value()
+
+        for i in range(1, circle_packing.num_circles + 1):
             x = ampl.var['x'][i].value()
             y = ampl.var['y'][i].value()
             self.circle_centers.append([x, y])
-        
-        self.radius = ampl.var['r'].value()
 
+        self.radius = ampl.var['r'].value()
 
     def plot_results(self):
         fig, ax = plt.subplots()
@@ -74,17 +78,15 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--solver", type=str, default="octeract", help="Solver to use: ipopt, baron, lgo, lindoglobal, octeract, couenne")
-    parser.add_argument("-n", "--circles", type=int, default=3, help="Number of circles")
     args = parser.parse_args()
 
     solver = args.solver
-    n = args.circles
 
     if solver not in CirclePacking.solver_lut:
         print(f"Solver {solver} not recognized.")
         print(f"Choose between: ipopt, baron, lgo, lindoglobal, octeract, couenne")
         exit()
 
-    circle_packing = CirclePacking(n=n, solver=solver)
+    circle_packing = CirclePacking(solver=solver)
     circle_packing.optimize()
     circle_packing.plot_results()
